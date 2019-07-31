d60e0d18cdbb13b63cca30c0412a1d9fd70fe81b
nvovap <nvovap@gmail.com>
2019-07-31 15:51:55 +0300

    first commit

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
+import './formStyles.css';
+
+class StreamCreate extends React.Component {
+
+  onSubmit = (formValues) => {
+    console.log('StreamCreate')
+    console.log(formValues)
+    
+    this.props.createStream(formValues);
+  }
+
+  render() {
+    return (
+     <div>
+       <h3>Create a Stream</h3>
+       <StreamForm parentSubmit={this.onSubmit} />
+     </div>
+    )
+  }
+}
+
+export default connect(null, { createStream })(StreamCreate);
---
+++ rtmpserver/index.js
@@ -0,0 +1,33 @@
+// https://habr.com/ru/company/ruvds/blog/457860/
+//ffmpeg -re -i "C:\Program Files\ffmpeg\2.mp4" -c copy -f flv rtmp://localhost/live/4.flv
+
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: 'C:\Program Files/ffmpeg/bin/ffmpeg.exe',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
